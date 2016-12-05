25326ad339ce24d51f2d06eec7fb825eb79ea06e
Sweepr <Sweepr@users.noreply.github.com>
2016-12-05 15:07:34 +0100

    Update Dao_Postgresql_Factory.php

--- lib/dao/Postgresql/Dao_Postgresql_Factory.php
+++ lib/dao/Postgresql/Dao_Postgresql_Factory.php
@@ -43,7 +43,7 @@
 	} # getUserDao
 
 	public function getCacheDao() {
-		return new Dao_Postgresql_Cache($this->_conn, $this->getCachePath());
+		return new Dao_Postgresql_Cache($this->_conn, $this->getCacheStore());
 	} # getCacheDao
 
 	public function getAuditDao() {
